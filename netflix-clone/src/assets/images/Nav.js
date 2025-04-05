4e743897090e711bb37c99c3f826d5914ea6b795
yordanos <2119yoyo@gmail.com>
2025-04-05 21:49:10 +0300

    new

---
+++ netflix-clone/src/assets/images/Nav.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+import { useHistory } from "react-router-dom";
+import { auth } from "../firebase";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  const history = useHistory();
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          onClick={() => history.push("/")}
+          className="nav__logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="Netflix Logo"
+        />
+        <img
+          onClick={() => auth.signOut()}
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="Avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
